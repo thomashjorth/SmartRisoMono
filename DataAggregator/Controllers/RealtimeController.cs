378070a53272ac32dee4abfa5cae7bf5e80b8fe6
tobra1 <tobiasbrasch@gmail.com>
2015-03-17 23:08:56 +0100

    Merge complete

--- DataAggregator/Controllers/RealtimeController.cs
+++ DataAggregator/Controllers/RealtimeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -11,8 +11,13 @@ namespace DataAggregator.Controllers
 		// GET: api/Realtime/5
 		public HttpResponseMessage Get(string id)
 		{
+<<<<<<< Upstream, based on origin/master
 			var response = Request.CreateResponse (HttpStatusCode.Created,Newtonsoft.Json.JsonConvert.SerializeObject(
 				WS.DownloadXML (id,"localhost","8085")));
+=======
+			var response = Request.CreateResponse (HttpStatusCode.Created, 
+				Newtonsoft.Json.JsonConvert.SerializeObject(WS.DownloadXML (id,"localhost","8085")));
+>>>>>>> 17800b6 Merge complete
 			//Double.Parse (Utils.WS.DownloadXML (id,"localhost","8085"))));
 			response.Headers.Add("Access-Control-Allow-Origin", "*");
 			response.Headers.Add("Access-Control-Allow-Methods", "GET");
