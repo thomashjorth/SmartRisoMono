37bc646f969cd8d78f7123e09ca354944fdeb8e9
tobra1 <tobiasbrasch@gmail.com>
2015-03-17 23:18:02 +0100

    merge conflict

--- DataAggregator/Controllers/RealtimeController.cs
+++ DataAggregator/Controllers/RealtimeController.cs
@@ -11,13 +11,8 @@ namespace DataAggregator.Controllers
 		// GET: api/Realtime/5
 		public HttpResponseMessage Get(string id)
 		{
-<<<<<<< Upstream, based on origin/master
 			var response = Request.CreateResponse (HttpStatusCode.Created,Newtonsoft.Json.JsonConvert.SerializeObject(
 				WS.DownloadXML (id,"localhost","8085")));
-=======
-			var response = Request.CreateResponse (HttpStatusCode.Created, 
-				Newtonsoft.Json.JsonConvert.SerializeObject(WS.DownloadXML (id,"localhost","8085")));
->>>>>>> 17800b6 Merge complete
 			//Double.Parse (Utils.WS.DownloadXML (id,"localhost","8085"))));
 			response.Headers.Add("Access-Control-Allow-Origin", "*");
 			response.Headers.Add("Access-Control-Allow-Methods", "GET");
