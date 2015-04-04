30b7b85e3a71c8d4f26feade62c03efecd263ed4
bostuh <ahmet_yildirim89@hotmail.com>
2015-04-04 11:26:36 +0200

    new commit

---
+++ MonDiaExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using MonDiaExample.Models;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace MonDiaExample.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View ();
+		}
+		
+	}
+}
+
---
+++ MonDiaExample/Models/Nagios.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MonDiaExample.Models
+{
+	public class Nagios
+	{
+		public String Status{ get; set; }
+		public String Name { get; set; }
+	}
+}
+
