8073b9272cfad9aa894b2c022b3607441a9dbdc5
thomashjorth <thomas.hjorth.hansen@gmail.com>
2015-04-22 10:42:49 +0200

    side

--- Visualization/Scripts/Controllers/sideBarController.js
+++ Visualization/Scripts/Controllers/sideBarController.js
@@ -1,3 +1,14 @@
 ﻿VisualizeApp.controller('sideBarController', ['$scope','$interval', '$http', 'AppService', function($scope, $interval, $http, AppService){
-    
+
+     $interval(function(){
+        var h=Math.floor(Date.now()/1000)-$scope.firstTime;
+        AppService.getData($scope.dataHost,$scope.dataPort,$scope.dataAggregation,$scope.dataResource)
+            .success(function (response){
+                if($scope.data.values.length > $scope.xTicks){
+                    $scope.data.values.shift();
+                }
+                $scope.data.values.push({timestamp: h, value: JSON.parse(response).value});
+                $scope.data = {config: {unit: $scope.title, xTicks: $scope.xTicks, yMin: $scope.valueMin, yMax: $scope.valueMax}, values: $scope.data.values};
+            });
+    }, 1000);
 }]);
