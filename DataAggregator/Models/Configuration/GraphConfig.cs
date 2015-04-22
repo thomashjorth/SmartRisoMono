cfacba3976cfb67e9944f1c6e55090d39d72438b
thomashjorth <thomas.hjorth.hansen@gmail.com>
2015-04-22 09:30:42 +0200

    Config classes move

---
+++ DataAggregator/Models/Configuration/GraphConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataAggregator
+{
+	public class GraphConfig : VisualizationConfig
+	{
+		public int XTicks, YMin, YMax;
+		public GraphConfig ( string host, int port, string aggregation, string resource, string titleHeading, int xTicks, int yMin, int yMax) : base(host, port, aggregation, resource,titleHeading)
+		{
+			VisualizationType = "d3Graph";
+			XTicks = xTicks;
+			YMin = yMin;
+			YMax = yMax;
+		}
+	}
+}
+
