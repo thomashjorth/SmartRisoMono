904c54e4b6e29cb76d7c6175aacda511c062f66e
unknown-user <unknown-user@10.16.136.9>
2015-04-23 15:58:55 +0200

    sd

--- Visualization/Scripts/Controllers/d3GaugeController.js
+++ Visualization/Scripts/Controllers/d3GaugeController.js
@@ -1,20 +1,27 @@
-﻿VisualizeApp.controller('d3GaugeController', ['$scope','$interval', '$http', 'AppService', function($scope, $interval, $http, AppService, d3GaugeDirective){
+VisualizeApp.controller('d3GaugeController', ['$scope','$interval', '$http', 'AppService', function($scope, $interval, $http, AppService, d3GaugeDirective){
 	$scope.firstTime = Math.floor(Date.now()/1000);
 	$scope.initialize = function(gauge)
   	{
         $scope.gauge = $scope.init.Gauges[gauge];
         
-        $scope.data={config: {label: $scope.gauge.Unit, min: $scope.gauge.ValueMin, max: $scope.gauge.ValueMax, PlaceHolder: $scope.gauge.ID},
+        $scope.data={config: {label: $scope.gauge.TitleHeading, min: $scope.gauge.ValueMin, max: $scope.gauge.ValueMax, PlaceHolder: $scope.gauge.ID},
             CompositeMeasurement: {v: 0,timestamp: $scope.firstTime} };
 
-        $scope.Resource = "?host="+$scope.gauge.DER.Host+"&port="+$scope.gauge.DER.Port
-            +"&wsInterface="+$scope.gauge.DER.Aggregation+"&resource="+$scope.gauge.DER.Resource;
-    
+        if($scope.gauge.DER != null){
+            $scope.Resource = "?host="+$scope.gauge.DER.Host+"&port="+$scope.gauge.DER.Port
+                +"&wsInterface="+$scope.gauge.DER.Aggregation+"&resource="+$scope.gauge.DER.Resource;
+        }else{
+            $scope.Resource = $scope.init.Resource;
+        }
         $interval(function(){
             var h=Math.floor(Date.now()/1000)-$scope.firstTime;
             AppService.getData($scope.gauge.Host,$scope.gauge.Port,$scope.gauge.Aggregation,$scope.Resource)
                 .success(function (response){
+<<<<<<< Upstream, based on origin/master
                 $scope.data={config: {label: $scope.gauge.Unit, min: $scope.gauge.ValueMin, max: $scope.gauge.ValueMax, PlaceHolder: $scope.gauge.ID}, 
+=======
+                $scope.data={config: {label: $scope.gauge.TitleHeading, min: $scope.gauge.ValueMin, max: $scope.gauge.ValueMax, PlaceHolder: $scope.gauge.ID}, 
+>>>>>>> 0e62d72 sd
                     CompositeMeasurement: {v: JSON.parse(response).value, timestamp:h} };
 
             });
--- Visualization/Scripts/Controllers/d3GraphController.js
+++ Visualization/Scripts/Controllers/d3GraphController.js
@@ -6,10 +6,22 @@ VisualizeApp.controller('d3GraphController', ['$scope','$interval', '$http', 'Ap
             {timestamp: new Date(),value: 0}
         ]};
 
+        if($scope.init.DER != null){
+            $scope.Resource = "?host="+$scope.init.DER.Host+"&port="+$scope.init.DER.Port
+                +"&wsInterface="+$scope.init.DER.Aggregation+"&resource="+$scope.init.DER.Resource;
+        }else{
+            $scope.Resource = $scope.init.Resource;
+        }
+
         $interval(function(){
+<<<<<<< Upstream, based on origin/master
 
           
             AppService.getData($scope.init.Host,$scope.init.Port,$scope.init.Aggregation,$scope.init.Resource)
+=======
+            var h=Math.floor(Date.now()/1000)-$scope.firstTime;
+            AppService.getData($scope.init.Host,$scope.init.Port,$scope.init.Aggregation,$scope.Resource)
+>>>>>>> 0e62d72 sd
                 .success(function (response){
                 if($scope.data.values.length > $scope.xTicks){
                     $scope.data.values.shift();
