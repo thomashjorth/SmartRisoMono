7ecb9f4844581689d2c1e99415169def174fd827
unknown-user <unknown-user@10.40.47.241>
2015-05-13 12:52:00 +0200

    are

---
+++ Visualization/Scripts/Controllers/tasksController.js
@@ -0,0 +1,26 @@
+VisualizeApp.controller('tasksController', ['$scope','$interval', '$http', 'AppService', function($scope, $interval, $http, AppService){
+    
+
+    $scope.params = $scope.init.Params + "&resource="
+
+
+	$scope.startUnit = function () {
+		AppService.putData($scope.init.Host,$scope.init.Port,$scope.init.Device,$scope.params+ "startLoad")
+            .success(function (){
+            	alert("success");
+            	$scope.message = "Unit started."
+            })
+            .error(function (error) {
+            	alert(error);
+            	$scope.message = "Error: unit not started."	
+            });
+	}
+
+	$scope.stopUnit = function () {
+		AppService.putData($scope.init.Host,$scope.init.Port,$scope.init.Device,$scope.params+"stopLoad")
+            .success(function (response){
+            	$scope.message = "Unit stoped."
+            });
+	}
+
+}]);
